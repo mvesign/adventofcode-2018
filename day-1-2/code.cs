725633da777d306506ef99400c5c2b19b0a46893
Unknown <git@mvesign.com>
2019-01-15 14:34:37 +0100

    And continue with day 1 part 2

---
+++ day-1-2/code.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace MVESIGN.AdventOfCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var frequencies = File.ReadAllLines(@"input.txt").Select(line => int.Parse(line)).ToArray();
+            var frequency = 0;
+            var results = new HashSet<int>();
+            for (int i = 0; ; i++)
+            {
+                if (!results.Add(frequency))
+                    break;
+                frequency += frequencies[i % frequencies.Length];
+            }
+
+            Console.WriteLine($"Frequency: {frequency}");
+        }
+    }
+}

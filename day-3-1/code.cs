3f7bc52ae8aeb7a34fc649002631d81e9bb1375f
Maikel Vreugde <git@mvesign.com>
2019-01-16 17:07:53 +0100

    Start of day 3 part 1, need to actually create the logic for it

---
+++ day-3-1/code.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace MVESIGN.AdventOfCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"input.txt");
+        }
+    }
+}

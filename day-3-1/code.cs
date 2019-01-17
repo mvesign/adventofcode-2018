1ad636360f7b40f107b33d9603560b4bc2e0bde3
Maikel Vreugde <git@mvesign.com>
2019-01-17 09:33:40 +0100

    And here is the functionality of day 3, part 1

--- day-3-1/code.cs
+++ day-3-1/code.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MVESIGN.AdventOfCode
 {
@@ -7,7 +10,34 @@ namespace MVESIGN.AdventOfCode
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines(@"input.txt");
+            var claims = File.ReadAllLines(@"input.txt");
+
+            var positions = new Dictionary<long, int>();
+
+            foreach (var claim in claims)
+            {
+                var matches = new Regex(@"^#\d+ @ (?<start>[0-9]+),(?<end>[0-9]+): (?<width>[0-9]+)x(?<height>[0-9]+)$")
+                    .Match(claim)
+                    .Groups;
+                
+                var start = int.Parse(matches["start"].Value);
+                var end = int.Parse(matches["end"].Value);
+                var width = int.Parse(matches["width"].Value);
+                var height = int.Parse(matches["height"].Value);
+
+                for (var i = start; i < start + width; i++)
+                {
+                    for (var j = end; j < end + height; j++)
+                    {
+                        var key = j * 10000 + i;
+                        if (!positions.ContainsKey(key))
+                            positions.Add(key, 0);
+                        positions[key]++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Square inches: {positions.Count(p => p.Value > 1)}");
         }
     }
 }
